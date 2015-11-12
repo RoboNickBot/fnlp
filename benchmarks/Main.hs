cbf15e75ba4e1cdbbb8eea901f7a617634bde97c
RoboNickBot <nicklewchenko92@gmail.com>
2015-11-11 19:43:52 -0600

    added benchmarks

--- benchmarks/Main.hs
+++ benchmarks/Main.hs
@@ -8,17 +8,23 @@ import Criterion.Main
 import Database.FNLP
 
 main :: IO ()
-main = prep >> defaultMain [ bench "build" (nfIO $ performBuild 10 
-                                                                "bench-test.sqlite3" 
-                                                                "test-sampsents")
-                           , bench "analize10" (nfIO $ analize "10" "bench10.sqlite3")
-                           , bench "analize30" (nfIO $ analize "30" "bench30.sqlite3")
-                           , bench "analize150" (nfIO $ analize "150" "bench150.sqlite3")
-                           , bench "identify10" (nfIO $ identify' "10" "bench10.sqlite3")
-                           , bench "identify30" (nfIO $ identify' "30" "bench30.sqlite3")
-                           , bench "identify150" (nfIO $ identify' "150" "bench150.sqlite3")]
+main = noprep >> defaultMain [ bench "build" (nfIO $ performBuild 10 
+                                                                  "bench-test.sqlite3" 
+                                                                  "test-sampsents")
+                             , bench "analize10" (nfIO $ analize "10" "bench10.sqlite3")
+                             , bench "analize30" (nfIO $ analize "30" "bench30.sqlite3")
+                             , bench "analize150" (nfIO $ analize "150" "bench150.sqlite3")
+                             , bench "analize400" (nfIO $ analize "400" "bench400.sqlite3")
+                             , bench "analize800" (nfIO $ analize "800" "bench800.sqlite3")
+                             , bench "identify10" (nfIO $ identify' "10" "bench10.sqlite3")
+                             , bench "identify30" (nfIO $ identify' "30" "bench30.sqlite3")
+                             , bench "identify150" (nfIO $ identify' "150" "bench150.sqlite3")
+                             , bench "identify400" (nfIO $ identify' "400" "bench400.sqlite3")
+                             , bench "identify800" (nfIO $ identify' "800" "bench800.sqlite3") ]
 
-prep = build 10 >> build 30 >> build 150 >> return ()
+noprep = return ()
+
+prep = build 150 >> build 400 >> build 800 >> return ()
 
 build size = do let file = "bench" ++ show size ++ ".sqlite3"
                 hPutStrLn stderr 
